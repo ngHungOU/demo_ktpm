c8f6c8f4fff16f11c4cc7d12a219cd009df2dcf7
hung nguyen <nguyenhung1572003@gmail.com>
2024-02-27 15:35:40 +0700

    commit to feature new

--- demo.cpp
+++ demo.cpp
@@ -7,9 +7,7 @@ int main()
   cout<<"hello world"<<endl;
   cout << "bug" << endl;
   cout << "another bug" << endl;
-<<<<<<< HEAD
   cout << "feature-new thay doi" << endl;
-=======
   cout << "main" << endl;
->>>>>>> 3a3e7d34f32716d198e4c551752a02d14fc58d7c
+
 }
